6cda4ac881887d4c08dc451ee343d03eefce9d20
Ahmed Rabie Mahmoud Mohamed <ahmed.rabie.mahmoud.96@gmail.com>
2023-11-04 06:11:45 +0200

    Necessary methods added to the user model
    These methods will be responsible for handling security issues such as
        - Encrypting the password
        - Updating the password
        - Hide user credentials
        - Generate Tokens
        - Login User

--- app/database/models/user.model.js
+++ app/database/models/user.model.js
@@ -100,6 +100,50 @@ const userSchema = mongoose.Schema({
   undeliveredMessages: [{}],
 });
 
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await bcryptjs.hash(this.password, 8);
+  }
+});
+userSchema.pre("findOneAndUpdate", async function (next) {
+  try {
+    if (this._update.password) {
+      const hashed = await bcryptjs.hash(this._update.password, 10);
+      this._update.password = hashed;
+      next();
+    }
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Hide user credentials data
+userSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  deletedElements = ["__v", "password", "tokens"];
+  deletedElements.forEach((element) => {
+    delete user[element];
+  });
+  return user;
+};
+
+userSchema.methods.generateToken = async function () {
+  const userData = this;
+  const token = jwt.sign({ _id: userData._id }, process.env.tokenPassword);
+  userData.tokens = userData.tokens.concat({ token });
+  await userData.save();
+  return token;
+};
+
+userSchema.statics.loginUser = async (email, password) => {
+  const userData = await User.findOne({ email });
+  if (!userData) throw new Error("invalid email");
+  const validatePassword = await bcryptjs.compare(password, userData.password);
+  if (!validatePassword) throw new Error("invalid password");
+  return userData;
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
